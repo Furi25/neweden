8fb55a93eee994831b356bdbb97c8547b8237338
Furi25 <paulwierda@gmail.com>
2015-10-06 17:05:10 +0200

    hoi

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  database: process.env.MONGO_URI || 'mongodb://localhost/nef'
+  database: process.env.MONGO_URI || 'mongodb://admin:1234@ds061757.mongolab.com:61757/newedenfaces-tutorial'
 };
